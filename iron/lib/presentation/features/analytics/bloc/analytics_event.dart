ccfd5001de2745b5128158fdb28da92bb1eeaae1
Himank <himanksharma8434@gmail.com>
2026-06-28 20:50:31 +0530

    refactor: remove deprecated service and associated test files

--- iron/lib/presentation/features/analytics/bloc/analytics_event.dart
+++
@@ -1,9 +0,0 @@
-import 'package:equatable/equatable.dart';
-
-abstract class AnalyticsEvent extends Equatable {
-  const AnalyticsEvent();
-  @override
-  List<Object?> get props => [];
-}
-
-class LoadAnalytics extends AnalyticsEvent {}
